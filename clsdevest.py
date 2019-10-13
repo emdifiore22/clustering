d0c4cacb1c811265c00da9d6e6353eedca21d01d
Emanuele Di Fiore <manu.difiore@gmail.com>
2019-10-13 13:22:45 +0200

    Devcalc.py
    Script for deviances

--- clsdevest.py
+++ clsdevest.py
@@ -1,26 +1,63 @@
 import json
+import sys
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-# read file
-with open('clustering_history.json', 'r') as myfile:
-    data = myfile.read()
 
-# parse file
-obj = json.loads(data)
-history = obj['prova']
+def cluster_deviance_estimation():
 
-# show values
-#print(str(history[len(history)-1]))
-max_distance = float(history[len(history)-1]['Distanza'])
-x = []
-y = []
-for hist_elem in history:
-    estimation = 100 * float(hist_elem['Distanza'])/max_distance
-    x.append(hist_elem['Numero di cluster'])
-    y.append(estimation)
-    print (hist_elem['Numero di cluster'] + "   " + hist_elem['Distanza'] + " ==> " + str(estimation))
+    user_input = []
 
-plt.plot(x,y)
-plt.xlabel('number of cluster')
-plt.ylabel('lost variance (% not exact)')
-plt.show()
+    for par in sys.argv:
+        user_input.append(par)
+
+    print ("### This script will find the number of cluster with maximum of " + user_input[1] + "% of lost deviance")
+    print ("### Add '-g' to get the associated graph")
+    print ("### (this is only an estimation and this values are not exact)")
+
+    # read file
+    with open('clustering_history.json', 'r') as myfile:
+        data = myfile.read()
+
+    # parse file
+    obj = json.loads(data)
+    history = obj['prova']
+
+    # show values
+    # print(str(history[len(history)-1]))
+    max_distance = float(history[len(history)-1]['Distanza'])
+
+    # axes
+    x = []
+    y = []
+
+    for hist_elem in history:
+        # estimation of lost deviance for each number of cluster
+        estimation = 100 * float(hist_elem['Distanza'])/max_distance
+        x.append(hist_elem['Numero di cluster'])
+        y.append(estimation)
+        # print values on command line
+        print (hist_elem['Numero di cluster'] + "   " + hist_elem['Distanza'] + "   ==> " + str(estimation))
+
+    i = 0
+    for prob in y:
+        if prob < float(user_input[1]):
+            last = i
+            # print("I found this: " + str(prob) + " ==> " + str(x[i]))
+        i += 1
+
+    print ("===> Estimation: " + str(y[last]) + " /" + str(x[last]))
+
+    if len(user_input) == 3:
+        if user_input[2] == "-g":
+            plt.plot(x, y)
+            plt.axis([0, 50, 0, 50])
+            plt.axvline(x=x[last], color='r')
+            plt.xlabel('number of cluster')
+            plt.ylabel('lost variance (% not exact)')
+            plt.show()
+
+
+cluster_deviance_estimation()
+
---
+++ devcalc.py
@@ -0,0 +1,94 @@
+import json
+import sys
+from math import pow
+import numpy as np
+
+user_input = []
+for par in sys.argv:
+    user_input.append(par)
+
+clusters = []
+for i in range(1, int(user_input[1])+1):
+    array_cl = []
+    clusters.append(array_cl)
+
+averages = []
+
+npcomponents = int(user_input[2])
+total_len = 0
+
+def read_file():
+    # read file
+    with open('cluster.json', 'r') as myfile:
+        data = myfile.read()
+
+    # parse file
+    obj = json.loads(data)
+    elements = obj['Cluster']
+    total_len = len(elements)
+
+    print "Numero di elementi: " + str(total_len)
+
+    for elem in elements:
+        for i in range(1, int(user_input[1])+1):
+            if int(elem['Cluster']) == i:
+                clusters[i-1].append(elem)
+
+# print to test the number of elements in each cluster
+# N.B.: clisters vector start from 0, cluster 1 elements are in clusters[0]
+
+   # for e in clusters:
+       # print len(e)
+
+def cluster_averages():
+
+    for cluster in clusters:
+        nelem = len(cluster)
+        #ncluster = cluster[0]['Cluster']
+        #print ("Elementi cluster " + ncluster + ": " + str(nelem))
+
+        # per ogni elemento del cluster
+        sum = [0] * npcomponents
+        avg = []
+        for elem in cluster:
+            for i in range(1, npcomponents+1):
+                sum[i-1] = sum[i-1] + float(elem['Principale'+str(i)])
+
+        for s in sum:
+            avg.append(s/int(nelem))
+        averages.append(avg)
+
+
+def intra_cluster_deviance():
+    intra_cluster = 0
+    for cluster in clusters:
+        ncluster = int(cluster[0]['Cluster'])
+        print ncluster
+        sum = 0
+        for elem in cluster:
+            for i in range(1, npcomponents+1):
+                sum = sum + pow(float(elem['Principale'+str(i)])-averages[ncluster-1][i-1], 2)
+        #print (sum)
+        intra_cluster = intra_cluster + sum
+
+    print intra_cluster
+
+def inter_cluster_deviance():
+    i = 0
+    inter_cluster = 0
+
+    for avg in averages:
+        nelem = len(clusters[i])
+        sum = 0
+        for x in avg:
+            sum = sum + pow(x, 2)
+        inter_cluster = inter_cluster + nelem * sum
+        i += 1
+
+    print inter_cluster
+
+
+read_file()
+cluster_averages()
+intra_cluster_deviance()
+inter_cluster_deviance()
